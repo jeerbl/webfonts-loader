dda6037ddf419f69388a9ef59ed1d96874ac3334
jeerbl <jb.jeromebrunel@gmail.com>
2017-02-07 18:04:46 +0100

    Fix path URL

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ module.exports = function (content) {
             content: res[format]
           }
         );
-        urls[format] = (pub + url).replace(/\\/g, '/');
+        urls[format] = path.join(pub, url).replace(/\\/g, '/');
         self.emitFile(url, res[format]);
       } else {
         urls[format] = 'data:' +
