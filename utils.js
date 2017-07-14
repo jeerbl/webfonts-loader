4f0f6251b457d2c4592cc223806aec26dc4ed3b8
duxiaofeng <297951292@qq.com>
2017-07-14 11:29:53 +0800

    format utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 var hashFiles = require('hash-files');
 
 module.exports = {
-    hashFiles: function (files, hashLength) {
-        hashLength = hashLength && +hashLength >= 8 && +hashLength <= 32 ? +hashLength : 20;
-        return hashFiles.sync({files: files}).slice(0, hashLength);
-    }
+  hashFiles: function (files, hashLength) {
+    hashLength = hashLength && +hashLength >= 8 && +hashLength <= 32 ? +hashLength : 20;
+    return hashFiles.sync({files: files}).slice(0, hashLength);
+  }
 };
