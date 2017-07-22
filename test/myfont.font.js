af8b97080e5b24505f0bd7792e6d50171eb40975
jeerbl <jb.jeromebrunel@gmail.com>
2017-07-21 20:24:42 -0400

    Rename test files

--- test/myfont.font.js
+++ test/myfont.font.js
@@ -2,9 +2,9 @@ module.exports = {
   'files': [
     './test-svg/*.svg'
   ],
-  'fontName': 'octicons',
-  'classPrefix': 'octicon-',
-  'baseSelector': '.octicon',
+  'fontName': 'myfonticons',
+  'classPrefix': 'myfonticon-',
+  'baseSelector': '.myfonticon',
   'types': ['eot', 'woff', 'woff2', 'ttf', 'svg'],
   'fixedWidth': true,
   'fileName': 'app.[fontname].[hash].[ext]'
