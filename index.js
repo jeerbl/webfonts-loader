98bcc2c35fb6362cfd29ab14b28910f226f2455d
Pablo Guimera <pablo.guimera@duedil.com>
2018-08-29 12:29:10 +0100

    Applied semistandard's syntax suggestions

--- index.js
+++ index.js
@@ -166,12 +166,13 @@ module.exports = function (content) {
       var format = formats[i];
       var filename = fontConfig.fileName || options.fileName || '[chunkhash]-[fontname].[ext]';
       var chunkHash = filename.indexOf('[chunkhash]') !== -1
-            ? hashFiles(generatorOptions.files, options.hashLength) : '';
+        ? hashFiles(generatorOptions.files, options.hashLength)
+        : '';
 
       filename = filename
-                  .replace('[chunkhash]', chunkHash)
-                  .replace('[fontname]', generatorOptions.fontName)
-                  .replace('[ext]', format);
+        .replace('[chunkhash]', chunkHash)
+        .replace('[fontname]', generatorOptions.fontName)
+        .replace('[ext]', format);
 
       if (!embed) {
         var formatFilename = loaderUtils.interpolateName(this,
